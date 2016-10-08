ba58d5736c9482ccce519cbad89641942932c89a
Mathias Lund <mathias@graduateland.com>
2016-10-08 10:22:46 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var exec = require('child_process').exec;
+
+app.get('/deploy', function (req, res) {
+
+  exec('bash deploy.sh',function(error,stdout,stderr) {
+    if (error) console.log(error);
+    process.stdout.write(stdout);
+    process.stderr.write(stderr);
+  });
+
+  res.send('Deploying!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+docker rm $(docker ps -a -q) -f
+docker rmi $(docker images -q) -f
+docker volume rm $(docker volume ls -qf dangling=true)
+docker-compose rm -f
+docker-compose pull
+docker-compose up --build -d
